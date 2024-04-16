c620823829cce9c25c04c9045f6c917b5288c0ce
Souvik Sarkar <souviksarkar260123@gmail.com>
2024-04-16 12:22:58 +0530

    create pages & bottom bar

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,17 @@ import SignupForm from "./_auth/forms/SignupForm";
 import AuthLayout from "./_auth/AuthLayout";
 import RootLayout from "./_root/RootLayout";
 import { Toaster } from "@/components/ui/toaster";
-import { Home } from "./_root/pages";
+import {
+  AllUsers,
+  CreatePost,
+  EditPost,
+  Explore,
+  Home,
+  PostDetails,
+  Profile,
+  Saved,
+  UpdateProfile,
+} from "./_root/pages";
 
 const App = () => {
   return (
@@ -19,6 +29,14 @@ const App = () => {
         {/* Private Routes */}
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/all-users" element={<AllUsers />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:id" element={<EditPost />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
+          <Route path="/profile/:id/*" element={<Profile />} />
+          <Route path="/update-profile/:id" element={<UpdateProfile />} />
         </Route>
       </Routes>
       <Toaster />
--- src/components/shared/BottomBar.tsx
+++ src/components/shared/BottomBar.tsx
@@ -1,8 +1,35 @@
+import { bottomBarLinks } from "@/constants";
+import { INavLink } from "@/types";
+import { Link, useLocation } from "react-router-dom";
 
 const BottomBar = () => {
+  const { pathname } = useLocation();
   return (
-    <div>Bottom bar</div>
-  )
-}
+    <section className="bottom-bar">
+      {bottomBarLinks.map((link: INavLink) => {
+        const isActive = pathname === link.route;
 
-export default BottomBar
+        return (
+          <Link
+            key={link.label}
+            to={link.route}
+            className={` ${isActive && "bg-primary-500 rounded-md "} flex-center flex-col gap-1 p-2 transition `}
+          >
+            <img
+              src={link.imgURL}
+              alt={link.label}
+              width={16}
+              height={16}
+              className={` group-hover:invert-white ${
+                isActive && "invert-white"
+              }`}
+            />
+            <p className="tiny-medium text-light-2">{link.label}</p>
+          </Link>
+        );
+      })}
+    </section>
+  );
+};
+
+export default BottomBar;
