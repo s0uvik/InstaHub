e5aad3a378678cbd757bd6755a1466ba2000cf01
Souvik Sarkar <94051534+s0uvik@users.noreply.github.com>
2024-04-28 11:40:43 +0530

    Update AuthLayout.tsx

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -8,16 +8,9 @@ const AuthLayout = () => {
       {isAUthenticated ? (
         <Navigate to="/" />
       ) : (
-        <>
           <section className="flex flex-1 flex-col justify-center items-center py-10">
             <Outlet />
           </section>
-          <img
-            src="/assets/images/side-img.svg"
-            alt="logo"
-            className=" hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
-          />
-        </>
       )}
     </>
   );
